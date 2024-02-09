530fe3b03b4442a9069b3146382eabbc6263efd8
Your Name <you@example.com>
2024-02-09 12:59:31 +0800

    frontend abit

--- backend/config.js
+++ backend/config.js
@@ -1,4 +1,4 @@
-const PORT = 3000;
+const PORT = 3333;
 
 const mongoDBURL = 'mongodb://localhost:27017/books-collection'
 module.exports = { 
---
+++ frontend/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import axios from 'axios'
+import Spinner from './components/spinner.js'
+import { Link } from 'react-router-dom'
+import { AiOutlineEdit } from 'react-icons/ai'
+import { BsInfoCircle } from 'react-icons/bs'
+import {MdOutlineAddBox, MdOutlineDelete} from 'react-icons/md'
+
+const Home = () => {
+    return(
+        <div>Home</div>
+    )
+}
+
+export default Home;
